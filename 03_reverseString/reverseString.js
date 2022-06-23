e8a2cddb11510f27303e371d90fda3d32930462e
Taylor Jardno <profjardno@gmail.com>
2022-06-23 15:50:22 -0700

    completed reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(x) {
+    var reversedString = x.split("").reverse().join("");
+    return reversedString;
 };
 
 // Do not edit below this line
