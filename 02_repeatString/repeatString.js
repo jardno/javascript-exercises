495e30e1c4e7227edb8fe8a4e1d436391cc99b5f
Taylor Jardno <profjardno@gmail.com>
2022-06-23 15:32:04 -0700

    relocated completed exercises to new forked repo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num<0) {
+        return "ERROR";
+    };
+    
+    var testString = "";
+    for (let i = 0; i < num; i++) {
+        testString += string;
+    };
+    return testString;
 };
 
 // Do not edit below this line
