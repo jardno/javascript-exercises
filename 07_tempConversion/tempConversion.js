496590eecd5852f4de453d9a5af68758e9e9fd84
Taylor Jardno <profjardno@gmail.com>
2022-06-24 12:10:09 -0700

    completed tempConversion exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  let tempC = (f - 32)*(5/9);
+
+  if (!Number.isInteger(tempC)) {
+    tempC = tempC.toFixed(1);
+  };
+
+  return Number(tempC);
 
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  let tempF = (c * 9/5) + 32;
+  
+  if (!Number.isInteger(tempF)) {
+    tempF = tempF.toFixed(1);
+  };
 
+  return Number(tempF);
 };
 
 // Do not edit below this line
