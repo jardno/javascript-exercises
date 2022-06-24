9b7d995da5f362bb098fa160c8bb09e8464906d2
Taylor Jardno <profjardno@gmail.com>
2022-06-24 11:41:48 -0700

    completed leapYears exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  
+    if (year % 4 == 0 && year % 10 !== 0) {
+        console.log(year+' is a leap year');
+        return true;
+    } else if (year % 16 == 0){
+        console.log(year+' is a leap year');
+        return true;
+      } else {
+        console.log(year+' is not a leap year');
+        return false;
+      };
 };
+  
 
 // Do not edit below this line
 module.exports = leapYears;
