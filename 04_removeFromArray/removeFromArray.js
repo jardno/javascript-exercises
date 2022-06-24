fa851dbb7dcb2661a749ae88441970848ace9761
Taylor Jardno <profjardno@gmail.com>
2022-06-24 10:07:00 -0700

    completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
-
+const removeFromArrayDELETE = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+            console.log(newArray);
+        }
+    })
+    return newArray;
 };
 
-// Do not edit below this line
+const removeFromArray = function(array, ...args) {
+    let checkedArray = [];
+    let valuesToCheck = Array.from(args);
+    console.log("Checking array for: "+valuesToCheck);
+  
+    for (let i = 0; i < valuesToCheck.length; i++) {
+        checkedArray = array.filter(a => a !== valuesToCheck[i]);
+        array = checkedArray;
+    };
+  
+    console.log("Filtered array: "+array);
+    return array;
+};
+
+// Do not edit below this line 
+
 module.exports = removeFromArray;
