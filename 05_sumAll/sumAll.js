59420c131749df94d8a612df3623534daf9e4b95
Taylor Jardno <profjardno@gmail.com>
2022-06-24 11:14:43 -0700

    completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let errorFound = false;
+  let errorMessage = 'ERROR';
+  
+  let sumTotal = 0;
+  
+  for (arg in args) {
+    if (typeof args[arg] !== 'number') {
+      errorFound = true;
+      console.log(errorMessage);
+      return errorMessage;
+    } else if (args[arg] < 0) {
+      errorFound = true;
+      console.log(errorMessage);
+      return errorMessage;
+    };
+  };
+  
+  while (errorFound == false) {
+    let sortedArgs = args.sort();
+    console.log(sortedArgs);
+    for (let i = 0; i < sortedArgs[1]; i++) {
+      sumTotal += sortedArgs[0] + i;
+    };
+    console.log('Sum: '+sumTotal);
+    return sumTotal;
+  };
+    
+  };
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
